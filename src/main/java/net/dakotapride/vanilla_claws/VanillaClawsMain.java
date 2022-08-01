6c18382f9edde9e438f11649f8dd6d11297aaf45
DakotaPride <rjtube13@gmail.com>
2022-08-01 09:56:07 -0500

    Initial commit

---
+++ src/main/java/net/dakotapride/vanilla_claws/VanillaClawsMain.java
@@ -0,0 +1,34 @@
+package net.dakotapride.vanilla_claws;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VanillaClawsMain.MOD_ID)
+public class VanillaClawsMain
+{
+    public static final String MOD_ID = "vanilla_claws";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public VanillaClawsMain()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
