e11cdb2d46b60ce1ebe45377d24d2b1bda7a50bf
DakotaPride <rjtube13@gmail.com>
2022-08-19 09:40:59 -0500

    Fixed Resource Slimes compatibility issue (1.19+)

--- src/main/java/net/dakotapride/vanilla_claws/mixin/LivingEntityMixin.java
+++ src/main/java/net/dakotapride/vanilla_claws/mixin/LivingEntityMixin.java
@@ -1,20 +1,14 @@
 package net.dakotapride.vanilla_claws.mixin;
 
 import net.dakotapride.vanilla_claws.registry.ItemsInit;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -22,7 +16,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
